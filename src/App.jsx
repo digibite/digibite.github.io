88f6ae2c92d76a7cd35662a1fa5ba43c4f9ad2c8
ig <ig@igs-MacBook-Pro.lan>
2024-08-12 16:53:39 -0700

    Center content

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <div>
+      <div className="container">
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
